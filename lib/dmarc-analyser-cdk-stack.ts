e69321e23cc97dcbcd27930b9d8d45e20f0df81c
Dylan Wilson <dwilson5817@gmail.com>
2026-04-05 21:13:15 +0000

    feat(cdk): add DynamoDB table, S3 bucket, and KMS key
    Add `ReportsTable` DynamoDB table, `RawReportsBucket` S3 bucket, and
    `EmailCredentialsKey` KMS key to enhance resource support in the stack.

--- lib/dmarc-analyser-cdk-stack.ts
+++ lib/dmarc-analyser-cdk-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DmarcAnalyserCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new kms.Key(this, 'EmailCredentialsKey', {
+      description: 'Key for encrypting email account credentials',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DmarcAnalyserCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new dynamodb.Table(this, 'ReportsTable', {
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    new s3.Bucket(this, 'RawReportsBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
