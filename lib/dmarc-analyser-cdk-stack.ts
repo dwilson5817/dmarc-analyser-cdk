a03b2ed56875cba0b97fe7912b6a63efcc53633b
Dylan Wilson <dwilson5817@gmail.com>
2026-04-05 21:36:26 +0000

    feat(cdk): integrate API Gateway and EventBridge with Lambda functions
    Add two constructs:
    
    - API Gateway + Lambda which will be used for the API which will handle
      requests for the frontend.
    - EventBridge + Lambda which will act essentially as a cron job to pull
      in the latest emails.

--- lib/dmarc-analyser-cdk-stack.ts
+++ lib/dmarc-analyser-cdk-stack.ts
@@ -1,12 +1,24 @@
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as events from 'aws-cdk-lib/aws-events';
 import * as kms from 'aws-cdk-lib/aws-kms';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import { ApiGatewayToLambda } from '@aws-solutions-constructs/aws-apigateway-lambda';
+import { EventbridgeToLambda } from '@aws-solutions-constructs/aws-eventbridge-lambda';
 import { Construct } from 'constructs';
 
 export class DmarcAnalyserCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const domainName = process.env.DOMAIN_NAME ?? 'api.dmarc.dylanw.dev';
+
+    new acm.Certificate(this, 'ApiCertificate', {
+      domainName,
+      validation: acm.CertificateValidation.fromDns(),
+    });
 
     new kms.Key(this, 'EmailCredentialsKey', {
       description: 'Key for encrypting email account credentials',
@@ -23,6 +35,25 @@ export class DmarcAnalyserCdkStack extends cdk.Stack {
     new s3.Bucket(this, 'RawReportsBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+    });
+
+    new EventbridgeToLambda(this, 'CronLambda', {
+      lambdaFunctionProps: {
+        runtime: lambda.Runtime.PYTHON_3_13,
+        handler: 'index.handler',
+        code: lambda.Code.fromInline('def handler(event, context):\n    pass\n'),
+      },
+      eventRuleProps: {
+        schedule: events.Schedule.rate(cdk.Duration.hours(1)),
+      },
+    });
+
+    new ApiGatewayToLambda(this, 'ApiLambda', {
+      lambdaFunctionProps: {
+        runtime: lambda.Runtime.PYTHON_3_13,
+        handler: 'index.handler',
+        code: lambda.Code.fromInline('def handler(event, context):\n    return {"statusCode": 200, "body": "ok"}\n'),
+      },
     });
   }
 }
